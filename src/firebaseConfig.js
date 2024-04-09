6ced253c49e120a72e78976e54d75d6ae350a832
Luis Moreno <luis.moreno1@catolica.edu.sv>
2024-04-09 14:21:33 -0600

    Se adaptó al nuevo proyecto

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXJoKG6sjrWdQc_UsPuDYq1rUN0GEsOvo",
-  authDomain: "db-sistema-pupuseria.firebaseapp.com",
-  projectId: "db-sistema-pupuseria",
-  storageBucket: "db-sistema-pupuseria.appspot.com",
-  messagingSenderId: "979871408805",
-  appId: "1:979871408805:web:9d298f0c8d7be28ffda34c"
+  apiKey: "AIzaSyASKWlUrYf2iAKEn16GAPQe_bzX8znWxoI",
+  authDomain: "db-sistema-gestor.firebaseapp.com",
+  projectId: "db-sistema-gestor",
+  storageBucket: "db-sistema-gestor.appspot.com",
+  messagingSenderId: "608726909500",
+  appId: "1:608726909500:web:c8485eb3fcf55da1573939"
 };
 
 // Initialize Firebase
