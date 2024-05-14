20632adde32670f37be6cdec89e7294fea4ffc3e
Luis Moreno <luis.moreno1@catolica.edu.sv>
2024-05-14 06:55:17 -0600

    Modificaciones de Bootstrap

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,9 +9,6 @@ import { useJornadaStore } from '../stores/jornada';
 // importando otros modulos
 import { fechaFormateada, nombreUsuario } from '../utilidades';
 
-// importando componentes de ui
-import ModalConfirmacion from './ModalConfirmacion.vue';
-
 // imagen del navbar
 import logoImg from '../assets/logoImg.jpg'
 
@@ -51,36 +48,41 @@ const fechaHoy = computed(() => {
         aria-labelledby="offcanvasNavbarLabel">
         <!-- Sidebar header -->
         <div class="offcanvas-header text-white border-bottom">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Pupusería</h5>
-          <span class="nav-link">Bienvenido {{ nombreUsuario(userStore.userData.email) }}</span>
-          <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas"
+          <router-link class="offcanvas-header text-white border-bottom text-decoration-none" active-class="active fw-bolder" to="/">
+            <img :src="logoImg" alt="" width="30" height="24">
+          </router-link>
+
+          <span class="nav-link text-dark">Bienvenido {{ nombreUsuario(userStore.userData.email) }}</span>
+          <button type="button" class="btn-close btn-close-dark shadow-none" data-bs-dismiss="offcanvas"
             aria-label="Close"></button>
         </div>
         <!-- Sidebar body -->
         <div class="offcanvas-body d-flex flex-lg-row flex-column p-4 p-lg-0">
           <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
-            <li class="nav-item mx-2">
-              <router-link class="nav-link" active-class="active fw-bolder" to="/mesas">Mesas</router-link>
+            <li class="nav-item mx-2 ">
+              <router-link class="nav-link text-dark" active-class="active fw-bolder" to="/mesas">Mesas</router-link>
             </li>
             <li class="nav-item mx-2">
-              <router-link class="nav-link" active-class="active fw-bolder" to="/ordenes">Órdenes</router-link>
+              <router-link class="nav-link text-dark" active-class="active fw-bolder" to="/ordenes">Órdenes</router-link>
             </li>
             <li class="nav-item mx-2">
-              <router-link class="nav-link" active-class="active fw-bolder" to="/menu">Menú</router-link>
+              <router-link class="nav-link text-dark" active-class="active fw-bolder" to="/menu">Menú</router-link>
             </li>
             <li class="nav-item mx-2">
-              <router-link class="nav-link" to="/administracion" v-if="userStore.userData.email === 'admin@test.com'"
+              <router-link class="nav-link text-dark" to="/administracion" v-if="userStore.userData.email === 'admin@test.com'"
                 active-class="active fw-bolder">Administración</router-link>
             </li>
           </ul>
           <!-- Login/ Sign up -->
           <span class="nav-link mt-lg-3 mb-lg-auto" v-if="jornadaStore.jornadaValor">Jornada activa en {{ fechaHoy }}</span>
           <div class="d-flex justify-content-center flex-lg-row flex-column align-items-center gap-3 mt-4">
+            
             <button class="btn text-white text-decoration-none rounded-4 ms-auto" style="background-color: #0069d9;"
               data-bs-toggle="modal" data-bs-target="#modalCerrarSesion">
               Salir
             </button>
-            <div class="modal fade" id="modalCerrarSesion" tabindex="-1" aria-labelledby="modalCerrarSesionLabel" aria-hidden="true">
+            <!-- Modal para cerrar sesion -->
+            <div class="modal fade" style="" id="modalCerrarSesion" tabindex="-1" aria-labelledby="modalCerrarSesionLabel" aria-hidden="true">
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header bg-primary text-white">
@@ -114,5 +116,4 @@ const fechaHoy = computed(() => {
   margin-top: 0.5rem !important;
   margin-bottom: 0.5rem !important;
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ import VueApexCharts from "vue3-apexcharts";
 import '@vuepic/vue-datepicker/dist/main.css';
 
 import "bootstrap/dist/css/bootstrap.min.css"
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "bootstrap"
-
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).use(router).use(createPinia()).use(VueApexCharts).component('VueDatePicker', VueDatePicker).mount('#app');
